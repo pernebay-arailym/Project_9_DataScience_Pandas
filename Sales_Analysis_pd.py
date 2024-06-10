82b22e6c34b4a126d2c57fad330fb2f8e6d916ff
pernebay-arailym <pernebayarailym@gmail.com>
2024-06-10 17:08:41 +0200

    first commit

---
+++ Sales_Analysis_pd.py
@@ -0,0 +1,9 @@
+#Import Nesessary Libraries
+import pandas as pd
+#Merging 12 months of sales data into a single CSV file
+# Using an absolute path example
+df = pd.read_csv("./Pandas-Data-Science-Tasks-master/SalesAnalysis/Sales_Data/Sales_April_2019.csv")
+
+
+
+df.head()
